13f52102747da5e5fa01932e3afa6579b7356216
Tazmondo <fondcoolman@outlook.com>
2022-07-26 11:52:16 +0100

    Adjust

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import * as React from "react"
+import React from "react"
 import {Link} from "gatsby";
 import * as styles from "../style/style";
 import Counter from "../components/counter";
@@ -9,8 +9,9 @@ const IndexPage = () => {
         <main style={styles.pageStyles}>
             <title>Home Page</title>
             <h1 style={styles.headingStyles}>My <span style={styles.headingAccentStyles}>Portfolio</span></h1>
-            <p style={styles.descriptionStyle}> {"WIP ->"} </p>
+            <p style={styles.descriptionStyle}> {"WIP"} </p>
             <Link style={styles.docLinkStyle} to={"/about"}>About me</Link>
+            <Link style={styles.docLinkStyle} to={"/game"}>Play a game</Link>
             <Counter/>
             <p><a style={styles.linkStyle} href="https://github.com/Tazmondo/">Back to my github</a></p>
         </main>
